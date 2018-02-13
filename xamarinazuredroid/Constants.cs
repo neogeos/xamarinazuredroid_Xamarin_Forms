1b33e377c1cdae85aebe2f3b8f8089e0bf7f1969
JW <JW@DESKTOP-SOGB1LT>
2018-02-13 11:57:50 -0430

    Agregar archivos de proyecto.

---
+++ xamarinazuredroid/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace xamarinazuredroid
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://xamarinazuredroid.azurewebsites.net";
+	}
+}
+
